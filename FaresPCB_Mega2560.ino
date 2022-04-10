1e1247cefc74244261fb31e259b3995aa31b042b
bassem_mostafa <bassem.ibrahim.ext@orange.com>
2022-04-10 17:57:39 +0200

    update demo application use Task LED

--- FaresPCB_Mega2560.ino
+++ FaresPCB_Mega2560.ino
@@ -3,6 +3,7 @@
 #include "LED.h"
 #include "Bluetooth.h"
 #include "LCD.h"
+#include "Task_LED.h"
 #include "Task_LCD.h"
 
 uint8_t buffer[20];
@@ -13,6 +14,7 @@ void setup()
     LED_TurnOff(LED_Internal);
     Bluetooth_Initialize();
     Bluetooth_Write((uint8_t*)"\nFaresPCB Ready\n", strlen("\nFaresPCB Ready\n"));
+    Task_LED_Initialize();
     Task_LCD_Initialize();
 }
 
@@ -51,23 +53,28 @@ void loop()
             switch (demo)
             {
                 case DEMO_1:
+                    Task_LED_Request(Task_LED_Request_On);
                     Task_LCD_Request(Task_LCD_Request_On);
                     Task_LCD_Request(Task_LCD_Request_Loading);
                     demo = DEMO_2;
                     break;
                 case DEMO_2:
+                    Task_LED_Request(Task_LED_Request_Off);
                     Task_LCD_Request(Task_LCD_Request_DateTime);
                     demo = DEMO_3;
                     break;
                 case DEMO_3:
+                    Task_LED_Request(Task_LED_Request_Blink);
                     Task_LCD_Request(Task_LCD_Request_Bluetooth);
                     demo = DEMO_4;
                     break;
                 case DEMO_4:
+                    Task_LED_Request(Task_LED_Request_Increase);
                     Task_LCD_Request(Task_LCD_Request_Temperature);
                     demo = DEMO_5;
                     break;
                 case DEMO_5:
+                    Task_LED_Request(Task_LED_Request_Decrease);
                     Task_LCD_Request(Task_LCD_Request_Distance);
                     demo = DEMO_6;
                     break;
