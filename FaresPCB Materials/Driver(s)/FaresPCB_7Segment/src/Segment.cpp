3fd067b3113c6d8ce33503144d7e9c0596ebae44
bassem_mostafa <bassem.ibrahim.ext@orange.com>
2022-04-23 12:13:18 +0200

    add 7-Segment

---
+++ FaresPCB Materials/Driver(s)/FaresPCB_7Segment/src/Segment.cpp
@@ -0,0 +1,134 @@
+// #############################################################################
+// #### Copyright ##############################################################
+// #############################################################################
+
+/*
+ * Copyright (C) 2022 BaSSeM
+ *
+ * This software is distributed under the terms and conditions of the 'Apache-2.0'
+ * license which can be found in the file 'LICENSE.txt' in this package distribution
+ * or at 'http://www.apache.org/licenses/LICENSE-2.0'.
+ */
+
+// #############################################################################
+// #### Description ############################################################
+// #############################################################################
+
+// #############################################################################
+// #### Include(s) #############################################################
+// #############################################################################
+
+#include "FaresPCB.h"
+#include "Segment.h"
+
+// #############################################################################
+// #### Private Macro(s) #######################################################
+// #############################################################################
+
+// #############################################################################
+// #### Private Type(s) ########################################################
+// #############################################################################
+
+// #############################################################################
+// #### Private Variable(s) ####################################################
+// #############################################################################
+
+// #############################################################################
+// #### Private Method(s) ######################################################
+// #############################################################################
+
+// #############################################################################
+// #### Public Method(s) #######################################################
+// #############################################################################
+
+/*
+ * @brief Writes to a 7-Segment
+ *
+ * @param[in] Segment   : selected 7-Segment
+ * @param[in] Digit     : digit to be written
+ *
+ * @return void     : None
+ */
+void Segment_Write( Segment_t Segment, Digit_t Digit )
+{
+    switch (Segment)
+    {
+        case Segment_1:
+        case Segment_2:
+        case Segment_3:
+        case Segment_4:
+            HW_PIN_OUTPUT(HW_7SEGMENT_BIT0);
+            HW_PIN_OUTPUT(HW_7SEGMENT_BIT1);
+            HW_PIN_OUTPUT(HW_7SEGMENT_BIT2);
+            HW_PIN_OUTPUT(HW_7SEGMENT_BIT3);
+            ( Digit & (0x01 << 0) ) ? HW_PIN_SET(HW_7SEGMENT_BIT0) : HW_PIN_CLEAR(HW_7SEGMENT_BIT0) ;
+            ( Digit & (0x01 << 1) ) ? HW_PIN_SET(HW_7SEGMENT_BIT1) : HW_PIN_CLEAR(HW_7SEGMENT_BIT1) ;
+            ( Digit & (0x01 << 2) ) ? HW_PIN_SET(HW_7SEGMENT_BIT2) : HW_PIN_CLEAR(HW_7SEGMENT_BIT2) ;
+            ( Digit & (0x01 << 3) ) ? HW_PIN_SET(HW_7SEGMENT_BIT3) : HW_PIN_CLEAR(HW_7SEGMENT_BIT3) ;
+            break;
+        default:
+            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT0);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT1);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT2);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT3);
+            break;
+    }
+    switch (Segment)
+    {
+        case Segment_1:
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
+            HW_PIN_CLEAR(HW_7SEGMENT_SEL1);
+            HW_PIN_SET(HW_7SEGMENT_SEL2);
+            HW_PIN_SET(HW_7SEGMENT_SEL3);
+            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            break;
+        case Segment_2:
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
+            HW_PIN_SET(HW_7SEGMENT_SEL1);
+            HW_PIN_CLEAR(HW_7SEGMENT_SEL2);
+            HW_PIN_SET(HW_7SEGMENT_SEL3);
+            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            break;
+        case Segment_3:
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
+            HW_PIN_SET(HW_7SEGMENT_SEL1);
+            HW_PIN_SET(HW_7SEGMENT_SEL2);
+            HW_PIN_CLEAR(HW_7SEGMENT_SEL3);
+            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            break;
+        case Segment_4:
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
+            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
+            HW_PIN_SET(HW_7SEGMENT_SEL1);
+            HW_PIN_SET(HW_7SEGMENT_SEL2);
+            HW_PIN_SET(HW_7SEGMENT_SEL3);
+            HW_PIN_CLEAR(HW_7SEGMENT_SEL4);
+            break;
+        default:
+            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL1);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL2);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL3);
+            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL4);
+            break;
+    }
+    _delay_ms(5);
+}
+
+// #############################################################################
+// #### Public Variable(s) #####################################################
+// #############################################################################
+
+// #############################################################################
+// #### END OF FILE ############################################################
+// #############################################################################
