646cfd4a66790bf41fd75cd161210b39b8be0df7
bassem <bmi.mostafa@gmail.com>
2022-10-18 15:19:43 +0200

    update LED update copy right year

--- FaresPCB Materials/Driver(s)/FaresPCB_LED/src/LED.cpp
+++ FaresPCB Materials/Driver(s)/FaresPCB_LED/src/LED.cpp
@@ -3,7 +3,7 @@
 // #############################################################################
 
 /*
- * Copyright (C) 2022 BaSSeM
+ * Copyright (C) 2023 BaSSeM
  *
  * This software is distributed under the terms and conditions of the 'Apache-2.0'
  * license which can be found in the file 'LICENSE.txt' in this package distribution
