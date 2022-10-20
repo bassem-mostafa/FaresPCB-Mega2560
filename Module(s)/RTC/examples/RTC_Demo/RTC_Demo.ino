9fdcf35c2c1cc2acb2caa6923a9a1dad3b2066d8
bassem <bmi.mostafa@gmail.com>
2022-10-20 14:45:12 +0200

    update RTC add Demo example

---
+++ Module(s)/RTC/examples/RTC_Demo/RTC_Demo.ino
@@ -0,0 +1,61 @@
+#include "Arduino.h"
+#include "Arduino_FreeRTOS.h"
+#include "string.h"
+
+#ifndef __TIMESTAMP__
+// Reference: https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
+#define __TIMESTAMP__ "??? ??? ?? ??:??:?? ????" /* Sun Sep 16 01:03:52 1973 */
+#endif
+
+#define _MONTH_IS(MMM) ( __TIMESTAMP__[4] == MMM[0] && __TIMESTAMP__[5] == MMM[1] && __TIMESTAMP__[6] == MMM[2] )
+#define _YEAR          ( (const char[]){__TIMESTAMP__[22], __TIMESTAMP__[23]} )
+#define _MONTH         ( _MONTH_IS("Jan") ? "01" : \
+                         _MONTH_IS("Feb") ? "02" : \
+                         _MONTH_IS("Mar") ? "03" : \
+                         _MONTH_IS("Apr") ? "04" : \
+                         _MONTH_IS("May") ? "05" : \
+                         _MONTH_IS("Jun") ? "06" : \
+                         _MONTH_IS("Jul") ? "07" : \
+                         _MONTH_IS("Aug") ? "08" : \
+                         _MONTH_IS("Sep") ? "09" : \
+                         _MONTH_IS("Oct") ? "10" : \
+                         _MONTH_IS("Nov") ? "11" : \
+                         _MONTH_IS("Dec") ? "12" : \
+                                            "??" )
+#define _DAY           ( (const char[]){__TIMESTAMP__[ 8], __TIMESTAMP__[ 9]} )
+#define _HOUR          ( (const char[]){__TIMESTAMP__[11], __TIMESTAMP__[12]} )
+#define _MINUTE        ( (const char[]){__TIMESTAMP__[14], __TIMESTAMP__[15]} )
+#define _SECOND        ( (const char[]){__TIMESTAMP__[17], __TIMESTAMP__[18]} )
+
+const char * _FW_LABEL = strrchr(__FILE__, '\\');
+const char _FW_VERSION[30] =
+{
+        _YEAR[0], _YEAR[1],
+        '.',
+        _MONTH[0], _MONTH[1],
+        '.',
+        _DAY[0], _DAY[1],
+        '.',
+        _HOUR[0], _HOUR[1],
+        '.',
+        _MINUTE[0], _MINUTE[1],
+        '.',
+        _SECOND[0], _SECOND[1],
+        '\0',
+};
+
+void setup()
+{
+    Serial.begin(115200);
+    while(!Serial);
+    Serial.print("FW ");
+    Serial.print(_FW_LABEL);
+    Serial.print(" ");
+    Serial.print(_FW_VERSION);
+    Serial.println("\n");
+}
+
+void loop()
+{
+    // TODO
+}
---
+++ Module(s)/RTC/examples/RTC_Demo/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-20 14:08:09
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "Arduino_FreeRTOS.h"
+#include "string.h"
+
+void setup() ;
+void loop() ;
+
+#include "RTC_Demo.ino"
+
+
+#endif
