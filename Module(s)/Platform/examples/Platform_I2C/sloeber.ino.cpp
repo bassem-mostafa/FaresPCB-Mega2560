feccb6443af55bcce0c03cfcb33745d0b6a3aca0
bassem <bmi.mostafa@gmail.com>
2022-10-21 22:03:33 +0200

    update platform add I2C example

---
+++ Module(s)/Platform/examples/Platform_I2C/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-21 21:00:49
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "stdio.h"
+
+void setup() ;
+void loop() ;
+
+#include "Platform_Demo.ino"
+
+
+#endif
