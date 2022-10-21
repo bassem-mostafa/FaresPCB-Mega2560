6c4669a1ef700134a0c896b4db32831c28bd3053
bassem <bmi.mostafa@gmail.com>
2022-10-21 10:43:30 +0200

    update platform add demo example

---
+++ Module(s)/Board/examples/Board_Demo/sloeber.ino.cpp
@@ -0,0 +1,19 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-21 10:36:11
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include "Arduino_FreeRTOS.h"
+#include "string.h"
+#include "Board.h"
+
+void setup() ;
+void loop() ;
+
+#include "Board_Demo.ino"
+
+
+#endif
