5dde44d1c96485c62a01aee6a2cc6ce31f813170
bassem <bmi.mostafa@gmail.com>
2022-10-22 15:19:55 +0200

    update LED update blink example

--- Module(s)/LED/examples/LED_Blink/sloeber.ino.cpp
+++ Module(s)/LED/examples/LED_Blink/sloeber.ino.cpp
@@ -2,11 +2,10 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-10-20 01:27:54
+//This file has been generated on 2022-10-22 15:09:56
 
 #include "Arduino.h"
-#include "Arduino.h"
-#include "Arduino_FreeRTOS.h"
+#include "Platform.h"
 #include "LED.h"
 #include "string.h"
 
