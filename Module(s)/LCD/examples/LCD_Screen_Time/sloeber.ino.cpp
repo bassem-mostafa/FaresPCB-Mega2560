eac4f02f73b07971904d55e1b277d95e78d18241
bassem <bmi.mostafa@gmail.com>
2022-10-25 12:42:48 +0200

    update LCD add time screen example

---
+++ Module(s)/LCD/examples/LCD_Screen_Time/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-25 12:41:29
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "LCD.h"
+
+void setup() ;
+void loop() ;
+
+#include "LCD_Screen_Time.ino"
+
+
+#endif
