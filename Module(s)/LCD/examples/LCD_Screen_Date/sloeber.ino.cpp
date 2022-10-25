fd726be8b3740fea7c8af0602b859b9dcb359bc5
bassem <bmi.mostafa@gmail.com>
2022-10-25 12:36:09 +0200

    update LCD add date screen example

---
+++ Module(s)/LCD/examples/LCD_Screen_Date/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-25 12:33:41
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "LCD.h"
+
+void setup() ;
+void loop() ;
+
+#include "LCD_Screen_Date.ino"
+
+
+#endif
