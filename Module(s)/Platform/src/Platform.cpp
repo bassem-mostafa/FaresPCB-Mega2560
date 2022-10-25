1c99f1a1597423bf08acbf0dad8528b5a68ac455
bassem <bmi.mostafa@gmail.com>
2022-10-25 20:54:41 +0200

    update platform udpate dummy platform implementation

--- Module(s)/Platform/src/Platform.cpp
+++ Module(s)/Platform/src/Platform.cpp
@@ -22,6 +22,8 @@
 // #### Include(s) #############################################################
 // #############################################################################
 
+#include "Platform.h"
+
 // #############################################################################
 // #### Private Macro(s) #######################################################
 // #############################################################################
@@ -45,6 +47,114 @@
 // #############################################################################
 // #### Public Method(s) #######################################################
 // #############################################################################
+
+__attribute((weak))
+Platform_Status_t Platform_Pin_Setup
+(
+        const Platform_Pin_t Platform_Pin,
+        const Platform_Pin_Mode_t Platform_Pin_Mode
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_Pin_Write
+(
+        const Platform_Pin_t Platform_Pin,
+        const Platform_Pin_Value_t Platform_Pin_Value
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_Pin_Write_PWM
+(
+        const Platform_Pin_t Platform_Pin,
+        const Platform_Pin_Value_PWM_t Platform_Pin_Value_PWM
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_Pin_Read
+(
+        const Platform_Pin_t Platform_Pin,
+        Platform_Pin_Value_t * const Platform_Pin_Value
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_I2C_Write
+(
+        const Platform_I2C_t Platform_I2C,
+        const Platform_I2C_Address_t Platform_I2C_Address,
+        const Platform_I2C_Data_t Platform_I2C_Data,
+        const Platform_I2C_Data_Length_t Platform_I2C_Data_Length
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_I2C_Read
+(
+        const Platform_I2C_t Platform_I2C,
+        const Platform_I2C_Address_t Platform_I2C_Address,
+        Platform_I2C_Data_t Platform_I2C_Data,
+        const Platform_I2C_Data_Length_t Platform_I2C_Data_Length
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_SPI_Transaction
+(
+        const Platform_SPI_t Platform_SPI,
+        Platform_SPI_Data_t Platform_SPI_Data,
+        const Platform_SPI_Data_Length_t Platform_SPI_Data_Length
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_USART_Write
+(
+        const Platform_USART_t Platform_USART,
+        const Platform_USART_Baudrate_t Platform_USART_Baudrate,
+        const Platform_USART_Data_t Platform_USART_Data,
+        const Platform_USART_Data_Length_t Platform_USART_Data_Length
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
+
+__attribute((weak))
+Platform_Status_t Platform_USART_Read
+(
+        const Platform_USART_t Platform_USART,
+        const Platform_USART_Baudrate_t Platform_USART_Baudrate,
+        Platform_USART_Data_t Platform_USART_Data,
+        const Platform_USART_Data_Length_t Platform_USART_Data_Length
+)
+{
+    Platform_Status_t Platform_Status = Platform_Status_NotSupported;
+    return Platform_Status;
+}
 
 // #############################################################################
 // #### Public Variable(s) #####################################################
