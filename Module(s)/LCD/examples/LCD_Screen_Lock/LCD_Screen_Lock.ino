1f00ec414314468fe12efaa326f04dee12abf3f6
bassem <bmi.mostafa@gmail.com>
2022-10-25 13:15:38 +0200

    update LCD add lock screen example

---
+++ Module(s)/LCD/examples/LCD_Screen_Lock/LCD_Screen_Lock.ino
@@ -0,0 +1,104 @@
+#include "Platform.h"
+#include "string.h"
+#include "LCD.h"
+
+#ifndef __TIMESTAMP__
+// Reference: https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
+#define __TIMESTAMP__ "??? ??? ?? ??:??:?? ????" /* Sun Sep 16 01:03:52 1973 */
+#endif
+
+#define _MONTH_IS(MMM) ( __TIMESTAMP__[4] == MMM[0] && __TIMESTAMP__[5] == MMM[1] && __TIMESTAMP__[6] == MMM[2] )
+#define _YEAR          ( (const char[]){__TIMESTAMP__[22], __TIMESTAMP__[23]} )
+#define _MONTH         ( _MONTH_IS("Jan") ? "01" : \
+                         _MONTH_IS("Feb") ? "02" : \
+                         _MONTH_IS("Mar") ? "03" : \
+                         _MONTH_IS("Apr") ? "04" : \
+                         _MONTH_IS("May") ? "05" : \
+                         _MONTH_IS("Jun") ? "06" : \
+                         _MONTH_IS("Jul") ? "07" : \
+                         _MONTH_IS("Aug") ? "08" : \
+                         _MONTH_IS("Sep") ? "09" : \
+                         _MONTH_IS("Oct") ? "10" : \
+                         _MONTH_IS("Nov") ? "11" : \
+                         _MONTH_IS("Dec") ? "12" : \
+                                            "??" )
+#define _DAY           ( (const char[]){__TIMESTAMP__[ 8], __TIMESTAMP__[ 9]} )
+#define _HOUR          ( (const char[]){__TIMESTAMP__[11], __TIMESTAMP__[12]} )
+#define _MINUTE        ( (const char[]){__TIMESTAMP__[14], __TIMESTAMP__[15]} )
+#define _SECOND        ( (const char[]){__TIMESTAMP__[17], __TIMESTAMP__[18]} )
+
+const char * _FW_LABEL = strrchr(__FILE__, '\\') + 1;
+const char _FW_VERSION[30] =
+{
+        _YEAR[0], _YEAR[1],
+        '.',
+        _MONTH[0], _MONTH[1],
+        '.',
+        _DAY[0], _DAY[1],
+        '.',
+        _HOUR[0], _HOUR[1],
+        '.',
+        _MINUTE[0], _MINUTE[1],
+        '.',
+        _SECOND[0], _SECOND[1],
+        '\0',
+};
+
+void setup()
+{
+    char FW_Info[50];
+    snprintf(FW_Info, sizeof(FW_Info), "\nFW %s V%s\n", _FW_LABEL, _FW_VERSION);
+    FW_Info[sizeof(FW_Info)-1] = '\0';
+    Platform_USART_Write
+    (
+            Platform_USART_USB,
+            Platform_USART_Baudrate_115200,
+            (uint8_t*)FW_Info,
+            strlen(FW_Info)
+    );
+    LCD_Setup(LCD_Custom_Character_1, LCD_Icon_LockOn);
+    LCD_Setup(LCD_Custom_Character_2, LCD_Icon_LockOff);
+    LCD_LightOn();
+}
+
+void loop()
+{
+    static LCD_Screen_t LCD_Screen_Lock =
+    {
+            "  Lock Screen   ",
+            {
+                    // "??? ??? ?? ??:??:?? ????"
+                    LCD_Custom_Character_1,
+                    ' ',
+                    ' ',
+                    ' ',
+                    ' ',
+                    'L',
+                    'o',
+                    'c',
+                    'k',
+                    'e',
+                    'd',
+                    ' ',
+                    ' ',
+                    ' ',
+                    ' ',
+                    ' ',},
+    };
+    switch (LCD_Screen_Lock[1][0])
+    {
+        case LCD_Custom_Character_1:
+            LCD_Screen_Lock[1][0] = LCD_Custom_Character_2;
+            memcpy(&LCD_Screen_Lock[1][1], "    Unlocked    ", strlen("    Unlocked    "));
+            break;
+        case LCD_Custom_Character_2:
+            LCD_Screen_Lock[1][0] = LCD_Custom_Character_1;
+            memcpy(&LCD_Screen_Lock[1][1], "     Locked     ", strlen("     Locked     "));
+            break;
+        default:
+            memcpy(&LCD_Screen_Lock[1][0], "     Error      ", strlen("     Error      "));
+            break;
+    }
+    LCD_Write(LCD_Screen_Lock);
+    _delay_ms(2000);
+}
---
+++ Module(s)/LCD/examples/LCD_Screen_Lock/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-25 13:13:09
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "LCD.h"
+
+void setup() ;
+void loop() ;
+
+#include "LCD_Screen_Lock.ino"
+
+
+#endif
