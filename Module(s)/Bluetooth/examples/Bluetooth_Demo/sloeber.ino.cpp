ec8cc7bfeb10effe36f5a8743fc2f5f470039b74
bassem <bmi.mostafa@gmail.com>
2022-10-25 13:54:06 +0200

    update bluetooth add demo example

---
+++ Module(s)/Bluetooth/examples/Bluetooth_Demo/sloeber.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-25 13:46:48
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "Bluetooth.h"
+
+void setup() ;
+void loop() ;
+
+#include "Bluetooth_Demo.ino"
+
+
+#endif
