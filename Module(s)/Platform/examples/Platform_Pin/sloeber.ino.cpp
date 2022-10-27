200d6d87275994fa3b6488622c1ce50beecf5ae4
bassem <bmi.mostafa@gmail.com>
2022-10-27 21:36:18 +0200

    update platform update pin example

--- Module(s)/Platform/examples/Platform_Pin/sloeber.ino.cpp
+++ Module(s)/Platform/examples/Platform_Pin/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-10-27 21:32:26
+//This file has been generated on 2022-10-27 21:34:34
 
 #include "Arduino.h"
 #include "Platform.h"
