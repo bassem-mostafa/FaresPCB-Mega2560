41f88d2bac9774f8acf0f3f314dab704a5b04917
bassem <bmi.mostafa@gmail.com>
2022-10-28 21:33:02 +0200

    update 7-segment update implementation

--- Module(s)/7Segment/src/Segment.cpp
+++ Module(s)/7Segment/src/Segment.cpp
@@ -18,8 +18,9 @@
 // #### Include(s) #############################################################
 // #############################################################################
 
-#include "FaresPCB.h"
+#include "Platform.h"
 #include "Segment.h"
+#include "util/delay.h"
 
 // #############################################################################
 // #### Private Macro(s) #######################################################
@@ -41,6 +42,54 @@
 // #### Private Method(s) ######################################################
 // #############################################################################
 
+bool _Segment_Initialize
+(
+        void
+)
+{
+    bool isInitialized = false;
+    do
+    {
+        if (isInitialized) break;
+        Platform_Pin_Setting_t Platform_Pin_Setting = NULL;
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_BIT0, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_BIT1, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_BIT2, Platform_Pin_Setting);
+
+        Platform_Pin_Setting_Initialize(&Platform_Pin_Setting);
+        Platform_Pin_Setting_Mode_Set(Platform_Pin_Setting, Platform_Pin_Mode_OUTPUT);
+        Platform_Pin_Setup(Platform_Pin_7SEGMENT_BIT3, Platform_Pin_Setting);
+        isInitialized = true;
+    }
+    while(0);
+    return isInitialized;
+}
+
 // #############################################################################
 // #### Public Method(s) #######################################################
 // #############################################################################
@@ -54,14 +103,11 @@
  */
 void Segment_Clear( void )
 {
-    HW_PIN_TRI_STATE(HW_7SEGMENT_SEL1);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_SEL2);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_SEL3);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_SEL4);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_BIT0);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_BIT1);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_BIT2);
-    HW_PIN_TRI_STATE(HW_7SEGMENT_BIT3);
+    _Segment_Initialize();
+    Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_HIGH);
+    Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_HIGH);
+    Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_HIGH);
+    Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_HIGH);
 }
 
 /*
@@ -74,75 +120,56 @@ void Segment_Clear( void )
  */
 void Segment_Write( Segment_t Segment, Digit_t Digit )
 {
+    _Segment_Initialize();
     switch (Segment)
     {
         case Segment_1:
         case Segment_2:
         case Segment_3:
         case Segment_4:
-            HW_PIN_OUTPUT(HW_7SEGMENT_BIT0);
-            HW_PIN_OUTPUT(HW_7SEGMENT_BIT1);
-            HW_PIN_OUTPUT(HW_7SEGMENT_BIT2);
-            HW_PIN_OUTPUT(HW_7SEGMENT_BIT3);
-            ( Digit & (0x01 << 0) ) ? HW_PIN_SET(HW_7SEGMENT_BIT0) : HW_PIN_CLEAR(HW_7SEGMENT_BIT0) ;
-            ( Digit & (0x01 << 1) ) ? HW_PIN_SET(HW_7SEGMENT_BIT1) : HW_PIN_CLEAR(HW_7SEGMENT_BIT1) ;
-            ( Digit & (0x01 << 2) ) ? HW_PIN_SET(HW_7SEGMENT_BIT2) : HW_PIN_CLEAR(HW_7SEGMENT_BIT2) ;
-            ( Digit & (0x01 << 3) ) ? HW_PIN_SET(HW_7SEGMENT_BIT3) : HW_PIN_CLEAR(HW_7SEGMENT_BIT3) ;
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT0, ( Digit & (0x01 << 0) ) ? Platform_Pin_Value_HIGH : Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT1, ( Digit & (0x01 << 1) ) ? Platform_Pin_Value_HIGH : Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT2, ( Digit & (0x01 << 2) ) ? Platform_Pin_Value_HIGH : Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT3, ( Digit & (0x01 << 3) ) ? Platform_Pin_Value_HIGH : Platform_Pin_Value_LOW);
             break;
         default:
-            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT0);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT1);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT2);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_BIT3);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT0, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT1, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT2, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_BIT3, Platform_Pin_Value_LOW);
             break;
     }
     switch (Segment)
     {
         case Segment_1:
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
-            HW_PIN_CLEAR(HW_7SEGMENT_SEL1);
-            HW_PIN_SET(HW_7SEGMENT_SEL2);
-            HW_PIN_SET(HW_7SEGMENT_SEL3);
-            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_HIGH);
             break;
         case Segment_2:
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
-            HW_PIN_SET(HW_7SEGMENT_SEL1);
-            HW_PIN_CLEAR(HW_7SEGMENT_SEL2);
-            HW_PIN_SET(HW_7SEGMENT_SEL3);
-            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_HIGH);
             break;
         case Segment_3:
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
-            HW_PIN_SET(HW_7SEGMENT_SEL1);
-            HW_PIN_SET(HW_7SEGMENT_SEL2);
-            HW_PIN_CLEAR(HW_7SEGMENT_SEL3);
-            HW_PIN_SET(HW_7SEGMENT_SEL4);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_LOW);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_HIGH);
             break;
         case Segment_4:
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL1);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL2);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL3);
-            HW_PIN_OUTPUT(HW_7SEGMENT_SEL4);
-            HW_PIN_SET(HW_7SEGMENT_SEL1);
-            HW_PIN_SET(HW_7SEGMENT_SEL2);
-            HW_PIN_SET(HW_7SEGMENT_SEL3);
-            HW_PIN_CLEAR(HW_7SEGMENT_SEL4);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_LOW);
             break;
         default:
-            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL1);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL2);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL3);
-            HW_PIN_TRI_STATE(HW_7SEGMENT_SEL4);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL1, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL2, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL3, Platform_Pin_Value_HIGH);
+            Platform_Pin_Write(Platform_Pin_7SEGMENT_SEL4, Platform_Pin_Value_HIGH);
             break;
     }
     _delay_ms(5);
