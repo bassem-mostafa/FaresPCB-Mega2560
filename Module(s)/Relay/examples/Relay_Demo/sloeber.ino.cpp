4b3010515aededf2093538f58559f68e4d5a3dd4
bassem <bmi.mostafa@gmail.com>
2022-10-28 20:45:43 +0200

    update Relay add demo example

---
+++ Module(s)/Relay/examples/Relay_Demo/sloeber.ino.cpp
@@ -0,0 +1,20 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-10-28 20:43:16
+
+#include "Arduino.h"
+#include "Platform.h"
+#include "string.h"
+#include "stdio.h"
+#include "Relay.h"
+#include "util/delay.h"
+
+void setup() ;
+void loop() ;
+
+#include "Relay_Demo.ino"
+
+
+#endif
