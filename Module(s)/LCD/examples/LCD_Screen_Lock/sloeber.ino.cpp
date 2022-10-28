503a8f11ad35103277241b1a854e8bb38a85a89b
bassem <bmi.mostafa@gmail.com>
2022-10-28 20:24:46 +0200

    update lcd update lock screen example

--- Module(s)/LCD/examples/LCD_Screen_Lock/sloeber.ino.cpp
+++ Module(s)/LCD/examples/LCD_Screen_Lock/sloeber.ino.cpp
@@ -2,12 +2,14 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-10-25 13:13:09
+//This file has been generated on 2022-10-28 20:17:27
 
 #include "Arduino.h"
 #include "Platform.h"
 #include "string.h"
+#include "stdio.h"
 #include "LCD.h"
+#include "util/delay.h"
 
 void setup() ;
 void loop() ;
