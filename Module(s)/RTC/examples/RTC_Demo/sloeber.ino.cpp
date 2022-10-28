176fbd02f68f900474e6b04842f97b2d22b180b8
bassem <bmi.mostafa@gmail.com>
2022-10-28 20:26:31 +0200

    update RTC update demo example

--- Module(s)/RTC/examples/RTC_Demo/sloeber.ino.cpp
+++ Module(s)/RTC/examples/RTC_Demo/sloeber.ino.cpp
@@ -2,11 +2,13 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-10-22 13:11:14
+//This file has been generated on 2022-10-28 20:11:06
 
 #include "Arduino.h"
 #include "Platform.h"
 #include "string.h"
+#include "stdio.h"
+#include "util/delay.h"
 #include "RTC.h"
 
 void setup() ;
